fe5ecf15ad79f4ffff39030233432cdae3579155
Navnitkumarsingh <54389534+Navnitkumarsingh@users.noreply.github.com>
2019-10-28 08:16:17 -0700

    Create binarysearch.c

---
+++ binarysearch.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int i, a[i],b,c,beg,end;
+printf("\n Enter number<=19\n");
+for(i=0;i<20;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("\n Enter no. for search\n");
+scanf("%d",&b);
+for(i=0;i<20;i++)
+{
+c=(beg+end)/2;
+if(c>b)
+{
+end=b-1;
+}
+else if
+beg=b+1;
+}
+else(c==b)
+{
+printf("\n search is successful\n");
+}
+}
